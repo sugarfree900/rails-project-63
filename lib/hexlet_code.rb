fa85681fe1ff245c5d35e157d5772a1b1f3726e6
Dmitriy Vershinin <dmitriy.vershinin@instamart.ru>
2022-10-28 01:20:04 +0200

    Implement form generation based on Tag class

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,11 +6,52 @@ require 'minitest/autorun'
 module HexletCode
   class Error < StandardError; end
 
-  User = Struct.new(:name, :job, keyword_init: true)
+  User = Struct.new(:name, :job, :gender, keyword_init: true) do
+    def initialize(params)
+      @form_value = ''
+      super(params)
+    end
+
+    def input(attr, params = {})
+      @form_value += case params[:as]
+                     when :text
+                       textarea_tag(attr, params)
+                     else
+                       default_tag(attr, params)
+                     end
+    end
+
+    def form_value
+      @form_value
+    end
+
+    private
+
+    def textarea_tag(attr, params)
+      Tag.build('textarea', {
+        name: attr,
+        cols: '20',
+        rows: '40'
+      }.merge(params.except(:as))) do
+        public_send(attr.to_s)
+      end
+    end
+
+    def default_tag(attr, params)
+      Tag.build('input', {
+        name: attr,
+        type: 'text',
+        value: public_send(attr.to_s)
+      }.merge(params))
+    end
+  end
 
   autoload(:Tag, './lib/tag.rb')
 
-  def self.form_for(_, params = {})
-    Tag.build('form', action: params[:url] || '#', method: 'post') {}
+  def self.form_for(user, params = {})
+    Tag.build('form', action: params[:url] || '#', method: 'post') do
+      yield user
+      user.form_value
+    end
   end
 end
--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/hexlet_code'
 
 class FormTest < Minitest::Test
   def setup
-    @user = HexletCode::User.new(name: 'rob')
+    @user = HexletCode::User.new name: 'rob', job: 'hexlet', gender: 'm'
   end
 
   def test_empty
@@ -14,5 +14,52 @@ class FormTest < Minitest::Test
   def test_users_url
     assert HexletCode.form_for(@user, url: '/users') { |_|
     } == '<form action="/users" method="post"></form>'
+  end
+
+  def test_with_inputs
+    result = HexletCode.form_for @user do |f|
+      # Проверяет есть ли значение внутри name
+      f.input :name
+      # Проверяет есть ли значение внутри job
+      f.input :job, as: :text
+    end
+    assert result == '<form action="#" method="post"><input name="name" type="text" value="rob"><textarea name="job" ' \
+                     'cols="20" rows="40">hexlet</textarea></form>'
+  end
+
+  # добавил в фикстуру value="hexlet" для атрибута job (тк значение у нас есть!)
+  def test_with_inputs_with_attrs
+    result = HexletCode.form_for @user, url: '#' do |f|
+      f.input :name, class: 'user-input'
+      f.input :job
+    end
+    assert result == '<form action="#" method="post"><input name="name" type="text" value="rob" class="user-input">' \
+                     '<input name="job" type="text" value="hexlet"></form>'
+  end
+
+  def test_with_inputs_with_default_values
+    result = HexletCode.form_for @user do |f|
+      f.input :job, as: :text
+    end
+    assert result == '<form action="#" method="post"><textarea name="job" cols="20" rows="40">hexlet</textarea></form>'
+  end
+
+  # изменил порядок атрибутов на тот, который прописан в дефолтах, тк в ruby порядок гарантируется хешем
+  def test_with_inputs_with_custom_values
+    result = HexletCode.form_for @user, url: '#' do |f|
+      f.input :job, as: :text, rows: 50, cols: 50
+    end
+    assert result == '<form action="#" method="post"><textarea name="job" cols="50" rows="50">hexlet</textarea></form>'
+  end
+
+  def test_error_for_undefined_field
+    assert_raises NoMethodError do
+      HexletCode.form_for @user, url: '/users' do |f|
+        f.input :name
+        f.input :job, as: :text
+        # Поля age у пользователя нет
+        f.input :age
+      end
+    end
   end
 end
