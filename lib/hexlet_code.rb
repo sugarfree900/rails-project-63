1f6c7ef0faf630c6db9a984e7a331deaa16aab90
Dmitriy Vershinin <dmitriy.vershinin@instamart.ru>
2022-11-02 09:15:22 +0100

    Revert "Decouple Rendering from Building Form"
    This reverts commit e52614cb6e304398cd5b10efc9765beb2a1df36a.

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,20 +6,61 @@ require 'minitest/autorun'
 module HexletCode
   class Error < StandardError; end
 
-  User = Struct.new(:name, :job, :gender, keyword_init: true)
+  User = Struct.new(:name, :job, :gender, keyword_init: true) do
+    def initialize(params)
+      @form_value = ''
+      super(params)
+    end
+
+    def input(attr, params = {})
+      @form_value += label_tag(attr)
+      @form_value += case params[:as]
+                     when :text
+                       textarea_tag(attr, params)
+                     else
+                       default_tag(attr, params)
+                     end
+    end
+
+    def submit(value = 'Save')
+      @form_value += Tag.build('input', { type: 'submit', value: value })
+    end
+
+    def form_value
+      @form_value
+    end
+
+    private
+
+    def textarea_tag(attr, params)
+      Tag.build('textarea', {
+        name: attr,
+        cols: '20',
+        rows: '40'
+      }.merge(params.except(:as))) do
+        public_send(attr.to_s)
+      end
+    end
+
+    def default_tag(attr, params)
+      Tag.build('input', {
+        name: attr,
+        type: 'text',
+        value: public_send(attr.to_s)
+      }.merge(params))
+    end
+
+    def label_tag(attr)
+      Tag.build('label', { for: attr }) { attr.to_s.capitalize }
+    end
+  end
 
   autoload(:Tag, './lib/tag.rb')
-  autoload(:Renderer, './lib/renderer.rb')
-  autoload(:Form, './lib/form.rb')
-  autoload(:Input, './lib/input.rb')
-  autoload(:LabelNode, './lib/label_node.rb')
-  autoload(:TextareaNode, './lib/textarea_node.rb')
-  autoload(:InputNode, './lib/input_node.rb')
-  autoload(:SubmitNode, './lib/submit_node.rb')
 
-  def self.form_for(user, params = {}, &block)
-    form = Form.new(user)
-    form.execute(&block)
-    Renderer.new(user, form.fields).render(params)
+  def self.form_for(user, params = {})
+    Tag.build('form', action: params[:url] || '#', method: params[:method] || 'post') do
+      yield user
+      user.form_value
+    end
   end
 end
