78166770bb273aaa7f1a49462437304a8a4a85f3
Dmitriy Vershinin <dmitriy.vershinin@instamart.ru>
2022-11-02 09:14:53 +0100

    Revert "Rename test file"
    This reverts commit 65e97c26a1a96416304c343c4359bc1f4ff7840e.

--- spec/form_spec.rb
+++ spec/form_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative '../lib/hexlet_code'
 
-class RendererTest < Minitest::Test
+class FormTest < Minitest::Test
   def setup
     @user = HexletCode::User.new name: 'rob', job: 'hexlet', gender: 'm'
     @setup ||= Dir['spec/fixtures/*'].map { |f| [File.basename(f, '.html'), File.read(f)] }.to_h
